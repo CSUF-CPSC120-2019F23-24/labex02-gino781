b00b9aadd8d753321b07514e10ee6c3bf127e037
Gino Angiuli <gino.angiuli@csu.fullerton.edu>
2019-08-31 15:20:33 -0700

    Bill Calculator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,21 @@
-// Name: 
+// Name:
 // This program calculates the tax and tip on a restaurant bill.
+
+#include <iostream>
+
+int main()
+{
+  double cost, tax;
+  int total;
+  
+  std::cout << "What is the total meal "cost"?";
+  std::cin >> 44.50;
+
+  tax = cost * 0.20;
+
+  total = tax + cost
+
+  std::cout << "The \"cost\" of the meal is 53.4./n";
+
+  return 0;
+}
