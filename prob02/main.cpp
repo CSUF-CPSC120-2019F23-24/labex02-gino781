ef1a592f5daf8bc5f7c49da2a654b83e1146ec90
Gino Angiuli <gino.angiuli@csu.fullerton.edu>
2019-08-31 14:37:59 -0700

    Length x Width

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -1,4 +1,4 @@
-// Name: 
+// Name:
 // This program calculates the area of a room.
 
 #include <iostream>
@@ -10,12 +10,12 @@ int main()
 
   // Get the length and width of the room.
   std::cout << "What is the room's "width" in feet? ";
-  std::cin >> width;
+  std::cin >> 11.5;
   std::cout << "What is the room's "length" in feet? ";
-  std::cin >> area;
+  std::cin >> 17.8;
 
   // Calculate the area of the room.
-  area = length / width;
+  area = length * width;
 
   // Display the area of the room.
   std::cout << "The \"area\" of the room is 204.7 square feet./n";
