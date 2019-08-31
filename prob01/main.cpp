747d0ab81b5bb40cf2ea2ef00a367bfd2918699c
Gino Angiuli <gino.angiuli@csu.fullerton.edu>
2019-08-31 14:16:23 -0700

    Diamond

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,10 @@
-// Name: 
+// Name:
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-
+  cout << "*\n ***\n *****\n *******\n *****\n ***\n *\n";
   return 0;
 }
